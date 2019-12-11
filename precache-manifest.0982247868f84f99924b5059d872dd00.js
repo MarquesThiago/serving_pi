ea20767db5e340180093dccd66f9c8a021513904
MarquesThiago <48632838+MarquesThiago@users.noreply.github.com>
2019-12-11 09:55:58 -0300

    serving

---
+++ precache-manifest.0982247868f84f99924b5059d872dd00.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a45a76f893cdc7e287a8075c8b003364",
+    "url": "/index.html"
+  },
+  {
+    "revision": "438b641604d53b7afb1f",
+    "url": "/static/css/main.b20e7ef1.chunk.css"
+  },
+  {
+    "revision": "a9e7a7bb2a8895cd9820",
+    "url": "/static/js/2.16ca2b9a.chunk.js"
+  },
+  {
+    "revision": "438b641604d53b7afb1f",
+    "url": "/static/js/main.9b1e00f0.chunk.js"
+  },
+  {
+    "revision": "1698f28f470c759ca997",
+    "url": "/static/js/runtime-main.9d930a46.js"
+  },
+  {
+    "revision": "29fde5d98221e3ca3b3a4500cdda2dcb",
+    "url": "/static/media/01.29fde5d9.jpg"
+  },
+  {
+    "revision": "ecd90f39e9189a66ac56f562bb21e3c7",
+    "url": "/static/media/01.ecd90f39.jpg"
+  },
+  {
+    "revision": "cd43916ff2d34ae98fc8575a8be24fad",
+    "url": "/static/media/02.cd43916f.jpg"
+  },
+  {
+    "revision": "5058158e38a32c57bafa67086ba23574",
+    "url": "/static/media/03.5058158e.jpg"
+  },
+  {
+    "revision": "75a4448406bda4f6ed596af696f1d5bc",
+    "url": "/static/media/04.75a44484.jpg"
+  },
+  {
+    "revision": "7f4ad72345e83aff705fa5204dd32a88",
+    "url": "/static/media/05.7f4ad723.jpg"
+  },
+  {
+    "revision": "7fb32cb650fe117e44b9ab650f844b43",
+    "url": "/static/media/2act_jg1.7fb32cb6.jpg"
+  },
+  {
+    "revision": "4b01e8ec660fd80a2b1f66ab8e0891d7",
+    "url": "/static/media/2act_tt1.4b01e8ec.jpg"
+  },
+  {
+    "revision": "cdc1baa2274b18c6b96b33e7cbfa8821",
+    "url": "/static/media/cake.cdc1baa2.jpg"
+  },
+  {
+    "revision": "875666900e9d6ecee973133413b814b6",
+    "url": "/static/media/coisas01.87566690.jpg"
+  },
+  {
+    "revision": "d67cd518d01b0f840c38c2f0aa7c9044",
+    "url": "/static/media/enc01.d67cd518.jpg"
+  },
+  {
+    "revision": "1cbdbe60b14ce64fad9e1a7935dc73d3",
+    "url": "/static/media/logo.1cbdbe60.png"
+  },
+  {
+    "revision": "105d23e8262537551eb21267ea6c492f",
+    "url": "/static/media/plant01.105d23e8.jpg"
+  },
+  {
+    "revision": "8e59ec5c9c6b881391b3c3c5af832610",
+    "url": "/static/media/reciclagem01.8e59ec5c.jpg"
+  }
+]);
